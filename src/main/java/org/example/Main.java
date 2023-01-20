1c4a792b207095ebfe6d4f9a6a554b0fe30f5eb1
Charlotte Bartholomé <charlotte.bartholome@gmail.com>
2023-01-20 16:30:23 +0100

    Initial commit

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,14 @@
+package org.example;
+
+import jakarta.persistence.EntityManagerFactory;
+import jakarta.persistence.Persistence;
+
+public class Main {
+    public static void main(String[] args) {
+
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("northwind");
+
+        emf.close();
+
+    }
+}
---
+++ src/main/java/org/example/entities/Product.java
@@ -0,0 +1,38 @@
+package org.example.entities;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "products")
+@Getter @Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class Product {
+    @Id
+    @Column(name = "product_id")
+    private long id;
+
+    @Column(name = "product_name")
+    private String name;
+
+    @Column(name = "quantity_per_unit")
+    private String qttPerUnit;
+
+    @Column(name = "units_in_stock")
+    private int stock;
+
+    @Column(name = "units_on_order")
+    private int onOrder;
+
+    @Column(name = "reorder_level")
+    private int reorderLevel;
+
+    private int discontinued;
+}
